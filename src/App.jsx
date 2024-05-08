da8f6a67e7aa0699379b1e5338d181fc43988b68
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2024-05-08 17:47:12 +0200

    moreDesignInPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import logo from './assets/logofrisor.png';
+import img from './assets/moreinfo.png';
+
 import './App.css'
 
-function App() {
-  
 
+function Content() {
   return (
-    <>
-      
-    </>
+    <div className="content">
+      <p>
+      "Välkommen till Frisör Plus Webbsida! Utforska vår digitala platform för en enkel tids bokning. Ladda ner
+      vår praktiska app för Android eller iOS för ett smidigt tids bokning för ditt
+      nästa klippning. Njut av en sömlös grooming-upplevelse, varje gång."
+      </p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <img class="logo" src={logo} alt="logo"/>
+      <Content />
+      <Timeinfo />
+      <img class="img" src={img} alt="img"/>
+    </div>
   )
+}
+
+function Timeinfo() {
+  return (
+    <div className="timeinfo">
+      <h2>Öpettider:</h2>
+<ul>
+      <li>Måndag 09:00 – 18:00</li>
+      <li>Tisdag 09:00 – 18:00</li>
+      <li>Onsdag 09:00 – 18:00</li>
+      <li>Torsdag 09:00 – 18:00</li>
+      <li>Fredag 09:00 – 18:00</li>
+      <li>Lördag 10:00 – 15:00</li>
+      </ul>
+      <p>📍Söndag Stäng!</p>
+    </div>
+  );
 }
 
 export default App
